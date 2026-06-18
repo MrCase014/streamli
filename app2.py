01b594a172d81537bb86fa2062aaa20583ec9251
MrCase014 <voundival@gmail.com>
2026-06-18 15:34:43 +0100

    add

--- app2.py
+++ app2.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 data = np.random.normal(size=1000)
-data = pd.DataFrame(data, columns=["Dist_nom"])
+data = pd.DataFrame(data, columns=["Dist_norm"])
+st.write(data.head())
+plt.hist(data.Dist_norm)
+st.pyplot()
